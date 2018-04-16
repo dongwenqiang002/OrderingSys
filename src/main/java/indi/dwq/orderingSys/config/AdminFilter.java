005ac6ffd28aae6f66aefeee14721d14d28ea3b5
董文强 <826112946@qq.com>
2018-04-16 18:05:30 +0800

    fghfghfg

--- src/main/java/indi/dwq/orderingSys/config/AdminFilter.java
+++ src/main/java/indi/dwq/orderingSys/config/AdminFilter.java
@@ -25,20 +25,29 @@ public class AdminFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         LOGGER.info("管理员验证");
+
         HttpServletRequest req= (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
         try {
             User user = (User) req.getSession().getAttribute("user");
+            LOGGER.info("当前用户为{},请求管理员访问登录",user.getRole());
             if(user == null){
                 resp.sendRedirect("/login.html");
             }
-            if(user.getRole().equals("管理员"));
-            chain.doFilter(request,response);
+            if(user.getRole().equals("管理员")) {
+                chain.doFilter(request, response);
+            }
         }catch (Exception e){
-            ((HttpServletResponse) response).setStatus(111);
+
+            //((HttpServletResponse) response).setStatus(111);
+            ((HttpServletResponse) response).sendError(111);
+            ((HttpServletResponse) response).sendRedirect("/error");
             return;
         }
-        ((HttpServletResponse) response).setStatus(111);
+       // ((HttpServletResponse) response).setStatus(111);
+        ((HttpServletResponse) response).sendError(111);
+        ((HttpServletResponse) response).sendRedirect("/error");
+        return;
     }
 
     @Override
